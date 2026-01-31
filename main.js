7fd13f5562f183de200783b23d2598945703c394
Imane Megouar <megouarimane@gmail.com>
2026-01-31 12:50:53 +0100

    i just modified style of header and add fetch function

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
 
-  console.log("JS loaded");
 
-  const searchInput = document.querySelector(".search-input");
-  console.log(searchInput);
 
-  searchInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-      console.log("User dreb Enter");
+
+// function  to fetch (search ) movie details from API
+ const getMovieDetails = async (query) => {
+    const myAPIKey = "233d663323afc329f230f615adeaeda0";
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${myAPIKey}&query=${encodeURIComponent(query)}`;
+    try{
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log(data);
     }
-  });
+    catch (error)
+    {
+      console.error("There has been a problem with your fetch operation:", error);
+    }
+  }
 
-});
+// addevent listener to search press enter key
+const searchInput = document.getElementById("searchInput"); 
+  
+searchInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const query = searchInput.value.trim();
+    if (query !== '') {
+      console.log(`Searching for: ${query}`);
+      getMovieDetails(query);
+    }
+
+  }
+})
